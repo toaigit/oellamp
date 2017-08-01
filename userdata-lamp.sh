b9fac8e2ecb2c70c2775088e2fce2c42659eb155
Toai Vo <toaivo@Toais-Mac-mini.local>
2017-08-01 08:50:29 -0700

    Packer - Build OEL LAMP

--- userdata-lamp.sh
+++ userdata-lamp.sh
@@ -7,7 +7,6 @@ sudo yum -y install php php-cli php-json php php-common php-gd php-pdo php-mysql
 sudo wget -O /tmp/mysql-5.7.11.noarch.rpm https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
 sudo yum -y install /tmp/mysql-5.7.11.noarch.rpm
 sudo yum -y install mysql-server
-sudo curl -o /tmp/postgres-9.6.noarch.rpm https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
-sudo yum -y install /tmp/postgres-9.6.noarch.rpm
+sudo yum -y install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-oraclelinux96-9.6-3.noarch.rpm
 sudo yum -y install postgresql96-server
 sudo /bin/timedatectl set-timezone America/Los_Angeles
